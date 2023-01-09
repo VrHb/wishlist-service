edb8b69ce4aa1a2add15779adfec2a8855be8781
Victor Abdukarimov <viredmi@gmail.com>
2023-01-09 18:03:33 +0300

    Add wish model

--- wishlists/wl_app/models.py
+++ wishlists/wl_app/models.py
@@ -1,2 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.deletion import CASCADE
+
+
+User = get_user_model()
+
+class Wish(models.Model):
+    '''Желание пользователя'''
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    title = models.CharField('Название желания', max_length=250)
+    text = models.TextField('Описание желания', blank=True, null=True)
+    link = models.CharField('Ссылка', max_length=300, blank=True, null=True)
+    price = models.DecimalField(
+        'Цена', 
+        max_digits=7, 
+        decimal_places=2,
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Желание'
+        verbose_name_plural = 'Желания'
+
+    def __str__(self):
+        return self.title
+
+
 
