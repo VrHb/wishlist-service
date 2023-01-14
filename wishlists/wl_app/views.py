ee08848159fb00c95cf6a6fa9bd0ad2fb462524a
Viktor Abdukarimov <viredmi@gmail.com>
2023-01-14 23:00:14 +0300

    Added params to wishes and iterate in template by list

--- wishlists/wl_app/views.py
+++ wishlists/wl_app/views.py
@@ -26,22 +26,29 @@ def show_wishlist(request, wishlist):
     if request.method == 'POST':
         wishlists = request.session.get('wishlists')
         if 'wishes' not in wishlists.get(wishlist):
-            wishlists.get(wishlist)['wishes'] = {}
+            wishlists.get(wishlist)['wishes'] = []
 
         # need generate wish id for wish key
         wish_title = request.POST['wish']
+        wish_link = request.POST['link']
+        wish_price = request.POST['price']
         if wish_title:
-            wishlists.get(wishlist)['wishes'][f"{wish_title}"] = {}
+            wishlists.get(wishlist)['wishes'] += [{
+                'title': wish_title,
+                'link': wish_link,
+                'price': wish_price,
+                'id': len(wishlists.get(wishlist)['wishes'])
+            }]
             request.session.save()
             return redirect(f'/{wishlist}')
     logger.info(request.session.items())
+    wishes = request.session.get('wishlists').get(wishlist).get('wishes')
     if request.GET.get('delete'):
-        wishes = request.session.get('wishlists').get(wishlist).get('wishes')
-        wishes.pop(request.GET['delete'])
+        wishes.pop(int(request.GET['delete']))
         request.session.save()
         return redirect(f'/{wishlist}')
     logger.info(request.GET)
-    return render(request, template_name="wishlist.html", context={'wishlist': wishlist, 'wishes': request.session.get('wishlists').get(wishlist).get('wishes')})
+    return render(request, template_name="wishlist.html", context={'wishlist': wishlist, 'wishes': wishes})
 
 
 def delete_wishlist(request, wishlist):
