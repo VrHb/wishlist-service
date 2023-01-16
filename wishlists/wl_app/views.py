cb1fe60fb106208702753a3993cc15955ca00cf2
Victor Abdukarimov <viredmi@gmail.com>
2023-01-16 13:56:41 +0300

    Changed data stucture and added id for urls

--- wishlists/wl_app/views.py
+++ wishlists/wl_app/views.py
@@ -10,51 +10,54 @@ logger = logging.getLogger(__name__)
 def show_main(request):
     if request.method == 'POST':
         if 'wishlists' not in request.session:
-            request.session['wishlists'] = {}
-        
-        # need generate wish id for wish key
+            request.session['wishlists'] = []
         wishlist_title = request.POST['wishlist']
         if wishlist_title:
-            request.session['wishlists'][f"{wishlist_title}"] = {"link": "", "Image": ""}
+            wishlist_id = len(request.session['wishlists'])
+            wishlist_params = {
+                'id': wishlist_id,
+                'title': wishlist_title,
+                'wishes': []
+                }
+            request.session['wishlists'].append(wishlist_params)
             request.session.save()
             return redirect('main')
     logger.info(logger.info(request.session.items()))
-    return render(request, template_name="index.html", context={"wishlists": request.session.get('wishlists')})
+    wishlists_params = {"wishlists": request.session.get('wishlists')}
+    return render(request, template_name="index.html", context=wishlists_params)
 
 
-def show_wishlist(request, wishlist):
+def show_wishlist(request, wishlist_id):
+    wishlist = request.session.get('wishlists')[wishlist_id]
     if request.method == 'POST':
-        wishlists = request.session.get('wishlists')
-        if 'wishes' not in wishlists.get(wishlist):
-            wishlists.get(wishlist)['wishes'] = []
         wish_title = request.POST['wish']
         wish_link = request.POST['link']
         wish_price = request.POST['price']
         if wish_title:
-            wishlists.get(wishlist)['wishes'] += [{
+            wishlist.get('wishes').append({
                 'title': wish_title,
                 'link': wish_link,
                 'price': wish_price,
-                'id': len(wishlists.get(wishlist)['wishes']),
+                'id': len(wishlist.get('wishes')),
 
                 # need give status from share list
                 'will_give': False, 
-            }]
+            })
             request.session.save()
-            return redirect(f'/{wishlist}')
+            return redirect(f'/{wishlist_id}')
     logger.info(request.session.items())
-    wishes = request.session.get('wishlists').get(wishlist).get('wishes')
+    wishes = wishlist.get('wishes')
     if request.GET.get('delete'):
         wishes.pop(int(request.GET['delete']))
         request.session.save()
-        return redirect(f'/{wishlist}')
+        return redirect(f'/{wishlist_id}')
     logger.info(request.GET)
-    return render(request, template_name="wishlist.html", context={'wishlist': wishlist, 'wishes': wishes})
+    wishlist_params = {'wishlist': wishlist, 'wishes': wishes}
+    return render(request, template_name="wishlist.html", context=wishlist_params)
 
 
-def delete_wishlist(request, wishlist):
-    wishes = request.session.get('wishlists')
-    wishes.pop(wishlist)
+def delete_wishlist(request, wishlist_id):
+    wishlists = request.session.get('wishlists')
+    wishlists.pop(wishlist_id)
     request.session.save()
     return redirect('main')
-
