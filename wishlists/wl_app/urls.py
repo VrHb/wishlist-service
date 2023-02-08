dee424a263b0d2e57a45258ba3fd39a9097e789c
Victor Abdukarimov <viredmi@gmail.com>
2023-02-08 23:10:20 +0300

    Fixed urls for share

--- wishlists/wl_app/urls.py
+++ wishlists/wl_app/urls.py
@@ -8,7 +8,7 @@ from . import views
 urlpatterns = [
     path('', views.show_main, name='main'),
     path('<int:wishlist_id>', views.show_wishlist, name='wishlist'),
-    path(f'share/<session_key>/<int:wishlist_id>', views.show_shared_wishlist, name='share_wishlist'),
+    path('share/<session_key>/<int:wishlist_id>', views.show_shared_wishlist, name='share_wishlist'),
     path('delete/<int:wishlist_id>', views.delete_wishlist, name='del_wishlist'),
     path('gifts', views.show_selected_gifts, name='gifts'),
     path('about', views.show_about, name='about'),
