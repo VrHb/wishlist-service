93d60cf345f749ba7c55b663c6347bd9551d6c16
Victor Abdukarimov <viredmi@gmail.com>
2023-05-12 21:48:41 +0300

    Add deploy script

---
+++ wishlists/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e 
+
+echo "Pulling changes from repository ..."
+git pull origin main
+
+echo "Activating virtual environment ..."
+source ./env/bin/activate
+
+echo "Installing python libs ..."
+pip install -r requirements.txt
+
+echo "Making db operations ..."
+python manage.py makemigrations --dry-run --check
+python manage.py migrate --no-input
+
+echo "Collecting static files ..."
+python manage.py collectstatic --no-input
+
+echo "Restarting app unit ..."
+sudo systemctl restart wishl.service
+
+echo "All done!"
