b1bb8daceb5d7871752d5200939738d35501fea6
Victor Abdukarimov <viredmi@gmail.com>
2023-05-17 19:20:12 +0300

    Some template fixes

--- wishlists/wl_app/admin.py
+++ wishlists/wl_app/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Wish
+from .models import Wish, Gift
 
 
 @admin.register(Wish)
 class WishAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Gift)
+class GiftAdmin(admin.ModelAdmin):
     pass
