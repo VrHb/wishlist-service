56afea63c1fe5a36eee63b4e60a06f19e0dfb566
Viktor Abdukarimov <viredmi@gmail.com>
2024-05-13 14:58:58 +0300

    Added tests

--- wishlists/wl_app/tests.py
+++ wishlists/wl_app/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExist
+
+
+@pytest.mark.django_db
+def test_create_and_check_user_in_db():
+    User = get_user_model()
+    User.objects.create(username='jack', password='some_pass')
+    user_from_db = User.objects.filter(username='jack').exists()
+    assert user_from_db
+
+
+@pytest.mark.django_db
+def test_raises_nonexist_object_exception():
+    User = get_user_model()
+    with pytest.raises(ObjectDoesNotExist):
+        User.objects.get(username='viti_a')
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("user_id", [999, 1000, 1001])
+def test_nonexist_user_raises_exception(user_id):
+    User = get_user_model()
+    with pytest.raises(ObjectDoesNotExist):
+        User.objects.get(id=user_id)
+
